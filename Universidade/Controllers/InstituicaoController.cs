114bc7bf77fa5ab45be89183737ed23f79564992
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2021-08-28 15:01:20 -0300

    Commit inicial. Inserido o modelo das instituições, bem como seu controlador e sua View. Ainda não foi implantado seu CRUD.

---
+++ Universidade/Controllers/InstituicaoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Universidade.Models;
+
+namespace Universidade.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Unimar",
+                Endereco = "Marília"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "Univem",
+                Endereco = "Marília"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 3,
+                Nome = "Católica",
+                Endereco = "Marília"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+    }
+}
